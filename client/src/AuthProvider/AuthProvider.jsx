3af220b1cd49b86d5c9584c285b531e070a2b224
marufhossain55 <engrmaruf3@gmail.com>
2024-11-16 17:19:21 +0600

    google sign in firebase function added

--- client/src/AuthProvider/AuthProvider.jsx
+++ client/src/AuthProvider/AuthProvider.jsx
@@ -3,18 +3,25 @@ import { app } from '../firebase/firebase.config';
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  GoogleAuthProvider,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
 } from 'firebase/auth';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const AuthProvider = () => {
+  const googleProvider = new GoogleAuthProvider();
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const login = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = () => {
+    return signInWithPopup(auth, googleProvider);
   };
 
   const logout = () => {
