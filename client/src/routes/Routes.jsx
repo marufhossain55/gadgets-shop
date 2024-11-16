04236884fa4a907b1805edf5db7960837a52109a
marufhossain55 <engrmaruf3@gmail.com>
2024-11-16 15:56:46 +0600

    routes added

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Home from '../pages/Home';
+import Products from '../pages/Products';
+import News from '../pages/News';
+import About from '../pages/About';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    children: [{ path: '/', element: <Home /> }],
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products', element: <Products /> },
+      { path: '/news', element: <News /> },
+      { path: '/about', element: <About /> },
+    ],
   },
 ]);
