62bb8f15388dc9abb55d92a9cbae8ebbc44926c2
marufhossain55 <engrmaruf3@gmail.com>
2024-11-16 17:13:21 +0600

    authprovider and create user function added

---
+++ client/src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext } from 'react';
+import { app } from '../firebase/firebase.config';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const AuthProvider = () => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  return <div>AuthProvider</div>;
+};
+export default AuthProvider;
